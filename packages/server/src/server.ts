87416a5924cdc225dfdbca32f4913f1ee9a15cd1
Jonas Kello <jonas.kello@gmail.com>
2019-04-22 15:08:34 +0200

    Add types

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import Keycloak from "keycloak-connect";
 
 const app = express();
 const port = 3001;
 
-const keycloak = new Keycloak();
+const keycloak = new Keycloak({});
+console.log("keycloak", keycloak);
 
 app.get("/api/hello-public", (_req, res) => res.send("Hello Public World!"));
 app.get("/api/hello-secure", (_req, res) => res.send("Hello Secure World!"));
